7ef219777a3f500ab14253953241f1b3da6cf1c3
Sadovsky33 <kacpersadik@gmail.com>
2025-03-11 13:55:45 +0100

    Dodaj pliki projektów.

---
+++ EgzaminProbnyConsole11.03.25/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("wprowadz a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("podaj b");
+int b = int.Parse(Console.ReadLine());
+//if (a != b)
+//{
+//    if (a > b)
+ //   {
+ //       if (a%2 == a - b )
+
+       // {
+
+
+
+
+     //   }
+
+
+
+   // }
+
+
+//}
+//else
+//{
+   // Console.WriteLine(a);
+//}
+
+
+while (a != b)
+{
+    if (a > b)
+    {
+        if (a % 2 == a - b)
+
+        {
+
+
+            return;
+
+        }
+
+    }
+    else
+    {
+     //  b % 2 == b - a
+
+    }
+
+}
+if (a == b)
+{
+    Console.WriteLine(a);
+}
